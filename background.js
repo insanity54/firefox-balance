a6f231b90d2b4a56c2dc84bd794fb65197ca3dbd
Chris Grimmett <chris@grimtech.net>
2018-02-13 00:45:53 -0800

    Update background.js

--- background.js
+++ background.js
@@ -7,3 +7,15 @@ browser.contextMenus.create({
   title: "Audio balance left",
   contexts: ["tab"]
 }, onCreated);
+
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  switch (info.menuItemId) {
+    case "balance-right":
+      console.log("tab audio balance set to RIGHT");
+      break;
+    case "balance-left":
+      console.log("tab audio balance set to LEFT");
+      break;      
+  }
+})
