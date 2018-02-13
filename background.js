7616c70c55db09b218a75509791a152b80f33af7
Chris Grimmett <chris@grimtech.net>
2018-02-13 01:14:24 -0800

    Update background.js

--- background.js
+++ background.js
@@ -13,6 +13,24 @@ browser.contextMenus.onClicked.addListener(function(info, tab) {
   switch (info.menuItemId) {
     case "balance-right":
       console.log("tab audio balance set to RIGHT");
+      var audioCtx = new (AudioContext || webkitAudioContext)();
+      var oscillator = audioCtx.createOscillator();
+      var panner = audioCtx.createStereoPanner();
+
+      oscillator.type = 'square';
+      oscillator.frequency.value = 440; // value in hertz
+      oscillator.connect(panner);
+      panner.connect(audioCtx.destination);
+      oscillator.start();
+
+      document.querySelector("input").oninput = function() {
+        panner.pan.value = +this.value; 
+      };
+
+      setInterval(function() {
+        oscillator.frequency.value = Math.round(Math.random() * 10) * 400 + 200;
+      }, 100);
+      
       break;
     case "balance-left":
       console.log("tab audio balance set to LEFT");
